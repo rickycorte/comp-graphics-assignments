50ce7271f5da7fd6df8e21d1fa78676b8411ce31
Riccardo <rickycortef@gmail.com>
2022-04-19 11:33:52 +0200

    Added Assignment 12

---
+++ A12/Assignment12.cpp
@@ -0,0 +1,108 @@
+// Following the Vulkan Tutorial until the
+// Drawing a triangle -> Setup -> Instance
+// section (currently at https://vulkan-tutorial.com/Drawing_a_triangle/Setup/Instance)
+// create a 640 x 480 window, with title "Assignment 12", and list the global
+// extensions supported by your O.S. 
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+
+const uint32_t WIDTH = 640;
+const uint32_t HEIGHT = 480;
+
+class Assignment12 
+{
+public:
+	// complete the Assignment class here 
+    void run() 
+    {
+        initWindow();
+
+        if(createIstance() != VK_SUCCESS)
+            throw std::runtime_error("failed to create instance!");
+
+        loop();
+
+        cleanup();
+    }
+
+
+private:
+    void initWindow()
+    {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Assignment 12", nullptr, nullptr);
+    }
+
+    VkApplicationInfo getAppInfo() 
+    {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Assignment 12";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        return appInfo;
+    }
+
+    VkResult createIstance()
+    {
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &getAppInfo();
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        return vkCreateInstance(&createInfo, nullptr, &instance);
+    }
+
+    void loop() 
+    {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup()
+    {
+        vkDestroyInstance(instance, nullptr);
+
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+
+    GLFWwindow* window;
+    VkInstance instance;
+    
+};
+
+int main() {
+    Assignment12 app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
