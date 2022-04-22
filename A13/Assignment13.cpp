c9927ae89c4ca7fdebf851652c03d418f71d486a
Riccardo <rickycortef@gmail.com>
2022-04-22 21:07:44 +0200

    Added assignment 13

---
+++ A13/Assignment13.cpp
@@ -0,0 +1,271 @@
+// Following the Vulkan Tutorial as shown in the enclose Assignment13.pdf, complete
+// this Vulkan initialization sample. You can copy and past code from Example E08,
+// or from other assginments such as Assignment0.cpp
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+#include <optional>
+#include <set>
+
+const uint32_t WIDTH = 640;
+const uint32_t HEIGHT = 480;
+
+class Assignment13 {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        initApp();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+	// place all the application variables here. As an example, here the one for the GLFW Window
+	// and the Vulkan instances are already defined.
+	
+    GLFWwindow* window;
+
+    VkInstance instance;
+
+    VkSurfaceKHR surface;
+
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    std::optional<uint32_t> aQueueWithGraphicsCapability;
+    std::optional<uint32_t> aQueueWithPresentationCapability;
+
+    VkDevice logicDevice;
+    float queuePriority = 1.0f;
+    VkQueue graphicsQueue;
+    VkQueue presentQueue;
+
+    VkCommandPool commandPool;
+
+    VkCommandBuffer commandBuffer;
+
+    // INIT WINDOW -----------------------------------------------------------------------------
+    void initWindow() {
+        glfwInit();
+
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Assignment 13", nullptr, nullptr);
+    }
+
+    // INIT INSTANCE -----------------------------------------------------------------------------
+    VkApplicationInfo getAppInfo()
+    {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Assignment 12";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        return appInfo;
+    }
+
+    VkResult createInstance()
+    {
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &getAppInfo();
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        return vkCreateInstance(&createInfo, nullptr, &instance);
+    }
+
+    // PICK PHISICAL -----------------------------------------------------------------------------
+    bool isDeviceSuitable(VkPhysicalDevice device) 
+    {
+
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        for (unsigned int i = 0; i < queueFamilyCount; i++) {
+            if ((queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) != 0) {
+                std::cout << " Graphics";
+                aQueueWithGraphicsCapability = i;
+            }
+            VkBool32 presentSupport = false;
+            vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+            if (presentSupport) {
+                aQueueWithPresentationCapability = i;
+            }
+        }
+
+        return aQueueWithGraphicsCapability.has_value() && aQueueWithPresentationCapability.has_value();
+    }
+
+    void pickPhysicalDevice()
+    {
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+        if (deviceCount == 0) {
+            throw std::runtime_error("failed to find GPUs with Vulkan support!");
+        }
+
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+        for (const auto& device : devices) 
+        {
+            if (isDeviceSuitable(device)) {
+                physicalDevice = device;
+                break;
+            }
+        }
+
+    }
+
+    // INIT LOGICAL -----------------------------------------------------------------------------
+    void createLogicalDevice()
+    {
+        VkPhysicalDeviceFeatures deviceFeatures{};
+        VkDeviceCreateInfo createInfo{};
+
+        std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+        std::set<uint32_t> uniqueQueueFamilies =
+        { aQueueWithGraphicsCapability.value(),
+        aQueueWithPresentationCapability.value() };
+
+        for (uint32_t queueFamily : uniqueQueueFamilies) {
+            VkDeviceQueueCreateInfo queueCreateInfo{};
+            queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            queueCreateInfo.queueFamilyIndex = queueFamily;
+            queueCreateInfo.queueCount = 1;
+            queueCreateInfo.pQueuePriorities = &queuePriority;
+            queueCreateInfos.push_back(queueCreateInfo);
+        }
+
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.pQueueCreateInfos = queueCreateInfos.data();
+        createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
+        createInfo.pEnabledFeatures = &deviceFeatures;
+        createInfo.enabledExtensionCount = 0;
+        createInfo.ppEnabledExtensionNames = nullptr;
+        createInfo.enabledLayerCount = 0;
+
+        auto result = vkCreateDevice(physicalDevice, &createInfo, nullptr, &logicDevice);
+
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("failed to create logical device!");
+        }
+
+        vkGetDeviceQueue(logicDevice, aQueueWithGraphicsCapability.value(), 0, &graphicsQueue);
+        vkGetDeviceQueue(logicDevice, aQueueWithPresentationCapability.value(), 0, &presentQueue);
+    }
+
+
+    // INIT CMD POOL -----------------------------------------------------------------------------
+    void initCommandPool()
+    {
+        VkCommandPoolCreateInfo poolInfo{};
+        poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+        poolInfo.queueFamilyIndex = aQueueWithGraphicsCapability.value();
+        poolInfo.flags = 0; // Optional
+        auto result = vkCreateCommandPool(logicDevice, &poolInfo, nullptr, &commandPool);
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("failed to create command pool!");
+        }
+    }
+
+    // INIT BUFFER -----------------------------------------------------------------------------
+    void initBuffer()
+    {
+        VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.commandPool = commandPool;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandBufferCount = 1;
+        auto result = vkAllocateCommandBuffers(logicDevice, &allocInfo, &commandBuffer);
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("failed to allocate command buffer!");
+        }
+    }
+
+    // INIT VULKAN -----------------------------------------------------------------------------
+    void initVulkan() {
+        if (createInstance() != VK_SUCCESS)
+            throw std::runtime_error("failed to create instance!");
+
+        // Prsentation Surface Creation
+        if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS)
+            throw std::runtime_error("failed to create window surface!");
+
+        // Physical Device selection
+        pickPhysicalDevice();
+
+        // Logical Device creation
+        createLogicalDevice();
+
+        // Command Pool creation
+        initCommandPool();
+
+        // Command Buffer creation
+        initBuffer();
+        
+    }
+
+
+    // APP -----------------------------------------------------------------------------
+
+	void initApp() {
+		// This will not be needed in this assignment!
+	}
+
+    void mainLoop() {
+    	// Wait for the window to close [you can take it from Assingment 12]
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+    	
+        vkEndCommandBuffer(commandBuffer);
+
+        vkDestroyCommandPool(logicDevice, commandPool, nullptr);
+
+        vkDestroyDevice(logicDevice, nullptr);
+
+        vkDestroySurfaceKHR(instance, surface, nullptr);
+
+        vkDestroyInstance(instance, nullptr);
+
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+};
+
+int main() {
+    Assignment13 app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
